ea8808f73abd389ff6efa63a9c174e4b566d605e
Beeahg <hoangn.ahg@gmail.com>
2025-07-26 22:00:26 +0700

    💬 Update AItrify chatbox UI gr4

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
- 
+  output: 'export',
+  images: {
+    unoptimized: true, // nếu bạn dùng <Image />
+  },
 };
 
 module.exports = nextConfig;
