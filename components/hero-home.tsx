bb9b68538efbd2820a563ecd63be84c9e1c7ead0
Beeahg <hoangn.ahg@gmail.com>
2025-07-31 18:50:16 +0700

    💬 Update AItrify chatbox UI v3.2

--- components/hero-home.tsx
+++ components/hero-home.tsx
@@ -30,7 +30,7 @@ export default function Hero({ agent }: { agent: string }) {
                 data-aos="fade-up"
                 data-aos-delay={200}
               >
-                AItrify tái định nghĩa Thương mại điện tử "eCommerce", nơi người Việt mua sắm trực tuyến
+                AItrify tái định nghĩa Thương mại điện tử "eCommerce", nơi dành cho người Việt mua sắm trực tuyến
                 với sự hỗ trợ của Trí tuệ nhân tạo AI, chúng tôi gọi đó là <strong className="font-bold text-indigo-100">AI.Commerce</strong>
               </p>
               <div className="mx-auto max-w-xs sm:flex sm:max-w-none sm:justify-center">
