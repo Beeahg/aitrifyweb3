b4fbfa734cc0df81733b822a03067370bf199dca
Beeahg <hoangn.ahg@gmail.com>
2025-07-31 18:50:16 +0700

    💬 Update AItrify chatbox UI v3.2

--- components/chatbox.tsx
+++ components/chatbox.tsx
@@ -14,13 +14,13 @@ const AGENT_CONFIGS: Record<string, { name: string; greeting: string; color: str
   anna: {
     name: 'ANNA',
     greeting:
-      'ANNA ở đây để giúp bạn tìm được tiện nghi như điều hòa và thiết bị gia dụng chính hãng nhưng giá rẻ nhất!',
+      'ANNA ở đây để giúp bạn tìm được tiện nghi như điều hòa và thiết bị gia dụng chính hãng kèm dịch vụ chuẩn hãng nhưng giá trong Top rẻ nhất!',
     color: 'bg-green-50 border-green-300',
   },
   lisa: {
     name: 'LISA',
     greeting:
-      'Chào Ông/Bà, LISA rất hân hạnh được trợ giúp về Golf, môn thể thao sang trọng với dịch vụ khách hàng xuất sắc dành cho người có điều kiện',
+      'Chào Ông/Bà, LISA rất hân hạnh được trợ giúp về Golf, môn thể thao sang trọng và phóng khoáng đi kèm với dịch vụ khách hàng xuất sắc dành cho người có điều kiện',
     color: 'bg-blue-50 border-blue-300',
   },
 };
